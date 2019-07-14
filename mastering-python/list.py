a81667e75ed1094be262218ca277295cca2e263b
Parham Alvani <parham.alvani@gmail.com>
2019-07-14 23:19:25 +0430

    chore: update list example

--- mastering-python/list.py
+++ mastering-python/list.py
@@ -1,25 +1,20 @@
-# In The Name Of God
-# ========================================
-# [] File Name : list.py
-#
-# [] Creation Date : 25-08-2016
-#
-# [] Created By : Parham Alvani (parham.alvani@gmail.com)
-# =======================================
-# List - a mutable list of items
+# list - a mutable list of items
 l1 = [1, 2, 3]
 print(l1)
 l1.append(4)
 print(l1)
 
+# tuple
 primes = (1, 2, 3, 5, 7)
 
-# List comprehension
+# convert a range into list
 numbers = list(range(10))
+# list comprehension
 not_primes = [number for number in numbers if number not in primes]
 print(not_primes)
 
-# Filter
+# convert a range into list
 numbers = list(range(10))
+# filter by another tuple
 not_primes = list(filter(lambda number: number not in primes, numbers))
 print(not_primes)
