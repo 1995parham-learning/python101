9dfbc0e44c9b0452cdafab728cad2f6d26939946
Parham Alvani <parham.alvani@gmail.com>
2022-12-03 23:22:42 +0330

    feat: update hello world of python

--- fun/hello.py
+++ fun/hello.py
@@ -1,14 +1,17 @@
+# working with expressions
 print("Hello world of python")
 print(1 + 1)
 print(1.0 + 1)
 parham = 1
 print(parham)
 
+# working with tuples
 a = ("Parham", "Navid", "Mohammad")
 b = (a, "Bahador")
 print(b)
 print(a.count("Parham"))
 
+# working with lists - 1
 a = ["Parham", "Navid", "Mohammad"]
 b = [a, "Bahador"]
 print(b)
@@ -17,18 +20,21 @@ print(a)
 a.sort()
 print(a)
 
+# working with lists - 2
 var = [10, 20, 30]
 c = (var, 10)
 print(c)
 var = var.append(40)
 print(c)
 
+# for-loop with else
 for name in a:
     print(name)
 else:
     print("loop finish correctly without break")
 
 
+# defining an inner-function, functions are first-class citizenes
 def retfunc(a):
     def f(b):
         print("%d %d" % (a, b))
