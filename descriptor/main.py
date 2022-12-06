cc77c0e23ef300cae9569a74b7d821a522193b2a
Parham Alvani <parham.alvani@gmail.com>
2022-12-06 11:29:54 +0330

    feat: update main example

--- descriptor/main.py
+++ descriptor/main.py
@@ -3,13 +3,22 @@ import abc
 
 
 class Alg(abc.ABC):
+    """
+    Abstract implementation of Alg algorithm.
+
+    Alg algorithm uses the information from its attached type.
+    it only attaches to Core class.
+    """
+
     def __init__(self):
         self.core: Core | None = None
 
     def __get__(self, obj: Core, objtype):
-        assert objtype is Core
-        self.core = obj
-        return self
+        if objtype is Core:
+            self.core = obj
+            return self
+        else:
+            raise AttributeError()
 
     def __call__(self):
         return self.run()
@@ -20,18 +29,34 @@ class Alg(abc.ABC):
 
 
 class Alg1(Alg):
+    """
+    First implementation of the Alg algorithm.
+    """
+
     def run(self):
         if self.core is not None:
             return f"Alg-1 from {self.core.name}"
 
 
 class Alg2(Alg):
+    """
+    Second implementation of the Alg algorithm.
+    """
+
     def run(self):
         if self.core is not None:
             return f"Alg-2 from {self.core.name}"
 
 
 class Core:
+    """
+    Core uses Alg algorithm and we wnat to control its implementation
+    of Alg alorithm.
+    """
+
+    # the descriptor must be in either the owner’s class dictionary
+    # or in the class dictionary for one of its parents
+    # by default core uses first implemtnation of Alg.
     alg: Alg = Alg1()
 
     def __init__(self, name: str = "default"):
@@ -44,6 +69,8 @@ c2 = Core("raha")
 assert c1.alg() == "Alg-1 from elahe"
 assert c2.alg() == "Alg-1 from raha"
 
+# sadlly this chnages implementation
+# on all instances.
 Core.alg = Alg2()
 
 assert c1.alg() == "Alg-2 from elahe"
