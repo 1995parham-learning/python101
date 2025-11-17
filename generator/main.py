d80947fc310a45bf422097521d041c540358f293
Elaheh Dastan <elahe.dstn@gmail.com>
2025-11-17 20:52:41 +0330

    Update main.py

--- generator/main.py
+++ generator/main.py
@@ -1,7 +1,22 @@
-# A Python generator is a special kind of function that lets you produce a sequence of values lazily — 
-# meaning it generates each value only when needed instead of building the entire list in memory.
+# A Python generator is a special kind of function that lets you produce a sequence of values lazily — meaning it generates each value only when needed 
+# instead of building the entire list in memory.
 
 # This makes generators great for:
+
 # Handling large datasets without memory issues
 # Creating infinite sequences
 # Writing cleaner, more readable code compared to manual iteration logic
+
+def count_up_to(n):
+    i = 1
+    while i <= n:
+        yield i
+        i += 1
+
+gen = count_up_to(3)
+
+next(gen)  # 1
+next(gen)  # 2
+next(gen)  # 3
+next(gen)  # StopIteration
+
